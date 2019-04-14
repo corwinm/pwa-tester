92d3d1f2db540bf0cede0452d3740f315839cc1b
Corwin Marsh <corwinm@users.noreply.github.com>
2019-04-14 16:14:11 -0700

    Cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,15 @@
 import React from "react";
 import preval from "preval.macro";
 
-const dateTimeStamp = preval`module.exports = new Date().toLocaleString();`
+const dateTimeStamp = preval`module.exports = new Date().toLocaleString();`;
 
 export default function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-          <strong>PWA Tester</strong> by{" "}
-          <a href="https://github.com/corwinm">Corwin Marsh</a>. The source code
-          is available on{" "}
+          PWA Tester by <a href="https://github.com/corwinm">Corwin Marsh</a>.
+          The source code is available on{" "}
           <a href="https://github.com/corwinm/pwa-tester">GitHub</a>
         </p>
         <p>Built: {dateTimeStamp}</p>
