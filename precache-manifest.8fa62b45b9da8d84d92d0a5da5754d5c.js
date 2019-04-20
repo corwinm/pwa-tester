5cb0e16e83630fd9c6610f6f5a0c4c6106e84bbf
Corwin Marsh <corwinm@users.noreply.github.com>
2019-04-20 12:20:19 -0700

    Updates

---
+++ precache-manifest.8fa62b45b9da8d84d92d0a5da5754d5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b33285a6b225118252d6",
+    "url": "/pwa-tester/static/css/main.aae92d82.chunk.css"
+  },
+  {
+    "revision": "b33285a6b225118252d6",
+    "url": "/pwa-tester/static/js/main.7744d42a.chunk.js"
+  },
+  {
+    "revision": "f68e8b37986ee7c3460a",
+    "url": "/pwa-tester/static/js/runtime~main.ea62272b.js"
+  },
+  {
+    "revision": "eb29434acedb5803a829",
+    "url": "/pwa-tester/static/js/2.41b98f98.chunk.js"
+  },
+  {
+    "revision": "377d4c30d1b2b437e5cf1d2905cdeb56",
+    "url": "/pwa-tester/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pwa-tester/precache-manifest.04f52ed0043236d87873bddbb3d3f58c.js"
+  "/pwa-tester/precache-manifest.8fa62b45b9da8d84d92d0a5da5754d5c.js"
 );
 
 workbox.clientsClaim();
