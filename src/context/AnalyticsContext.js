7b8ebf2c3bc761f5bcd1a5de28bca22ef2579a10
Corwin Marsh <corwinm@users.noreply.github.com>
2021-04-27 23:18:44 -0700

    Cleanup analytics and re-enable sw

--- src/context/AnalyticsContext.js
+++ src/context/AnalyticsContext.js
@@ -1,8 +1,10 @@
 import ReactGA from "react-ga";
 
-ReactGA.initialize("UA-139919526-1", {
-  testMode: process.env.NODE_ENV !== "production",
-});
+export function initializeGA() {
+  ReactGA.initialize("UA-139919526-1", {
+    testMode: process.env.NODE_ENV !== "production",
+  });
+}
 
 export function pageView(path) {
   ReactGA.pageview(process.env.PUBLIC_URL + path);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import "./index.scss";
 import App from "./components/App";
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { initializeGA } from './context/AnalyticsContext';
+
+initializeGA();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,9 +19,21 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.register();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
+
+function sendToAnalytics({ id, name, value }) {
+  ReactGA.ga('send', 'event', {
+    eventCategory: 'Web Vitals',
+    eventAction: name,
+    eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    eventLabel: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate
+  });
+}
+
+reportWebVitals(sendToAnalytics);
